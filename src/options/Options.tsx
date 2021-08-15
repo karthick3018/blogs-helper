f925480fa545ae4d36723ba3204241599f36dacb
karthick3018 <karthickraja3018@gmail.com>
2021-08-15 20:41:01 +0530

    Initialize project using Create React App

---
+++ src/options/Options.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './Options.css';
+
+function Options() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/options/Options.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Options;
